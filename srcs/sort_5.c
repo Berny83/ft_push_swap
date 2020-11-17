96f08b394522ce15c5864d2a42cc2d321b002dc8
Achacha Agrivane <aagrivan@at-e1.msk.21-school.ru>
2020-11-17 16:20:12 +0300

    sort 5 - added some protection

--- srcs/sort_5.c
+++ srcs/sort_5.c
@@ -6,12 +6,28 @@
 /*   By: aagrivan <aagrivan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 11:54:24 by aagrivan          #+#    #+#             */
-/*   Updated: 2020/11/13 19:13:01 by aagrivan         ###   ########.fr       */
+/*   Updated: 2020/11/14 12:26:27 by aagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_struct.h"
 #include "push_swap.h"
+
+static t_bool	is_sorted_3(t_data *d)
+{
+	t_elem		*curr;
+
+	curr = d->a.first;
+	if (!d->a.first)
+		return (false);
+	while (curr && curr->next)
+	{
+		if (curr->num > curr->next->num)
+			return (false);
+		curr = curr->next;
+	}
+	return (true);
+}
 
 static void	make_moves_5(t_data *d, int small, char c)
 {
@@ -56,16 +72,19 @@ static void	sort_5_extend(t_data *d)
 
 void		sort_3(t_data *d)
 {
-	if ((d->a.first->num > d->a.first->next->num &&\
-	d->a.last->num < d->a.first->next->num) ||\
-	d->a.first->num < d->a.last->num)
-		sa(d, 'a');
-	if (d->a.first->num > d->a.last->num &&\
-	d->a.first->num > d->a.first->next->num)
-		ra(d, 'a');
-	if (d->a.first->num > d->a.last->num &&\
-	d->a.first->num < d->a.first->next->num)
-		rra(d, 'a');
+	if (!is_sorted_3(d))
+	{
+		if ((d->a.first->num > d->a.first->next->num &&\
+		d->a.last->num < d->a.first->next->num) ||\
+		d->a.first->num < d->a.last->num)
+			sa(d, 'a');
+		if (d->a.first->num > d->a.last->num &&\
+		d->a.first->num > d->a.first->next->num)
+			ra(d, 'a');
+		if (d->a.first->num > d->a.last->num &&\
+		d->a.first->num < d->a.first->next->num)
+			rra(d, 'a');
+	}
 }
 
 void		sort_5(t_data *d)
